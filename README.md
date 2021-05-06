3f89dfd405739104652768c5b10a88f6c18bfec5
Jeff Kunce <jjkunce@gmail.com>
2021-05-06 15:47:35 -0400

    implement proxy to pskreporter API

--- README.md
+++ README.md
@@ -15,3 +15,9 @@ npm run start
  - This will start the server on port 3000
    - to use other port, set env var PORT
  - js8path app will be at http://localhost:3000/js8path-vue
+
+## implementation notes
+
+Proxy server implementation based on https://www.twilio.com/blog/node-js-proxy-server
+
+
--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+/**
+ * values from environment
+ */
+const PSKREPORTER_API_URL = process.env.PSKREPORTER_API_URL
+  || 'https://retrieve.pskreporter.info/query';
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +23,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Authorization
+/*
+app.use('', (req, res, next) => {
+  if (req.headers.authorization) {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+});
+*/
+
+// Proxy endpoints
+app.use('/api/pskreporter', createProxyMiddleware({
+  target: PSKREPORTER_API_URL,
+  changeOrigin: true,
+  pathRewrite: {
+    [`^/api/pskreporter`]: '',
+  },
+}));
 
 module.exports = app;
